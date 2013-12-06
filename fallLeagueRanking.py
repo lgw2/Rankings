2f7c28c004991e26159b1075c2fc6973d7b3b85b
Lucy <lgw2@uw.edu>
2013-12-05 23:54:27 -0800

    adding ipynb

--- fallLeagueRanking.py
+++
@@ -1,6 +0,0 @@
-from numpy import matrix
-teamList = ["Piehole", "Ghetto Birds", "Whiskey Tango Foxtrot", "Eats, Throws, and Leaves", "Jazz Picnic", "Toolbox", "Blue Footed Boobies"]
-print(teamList)
-len(teamList)
-winMatrix = matrix([0,1,1,1,1,1,1],[0,0,1,1,1,1,1],[0,0,0,0,1,1,1],[0,0,1,0,0,1,1],[1,0,0,1,0,1,0],[0,0,0,0,0,0,1],[0,0,0,0,1,0,0])
-print(winMatrix)
