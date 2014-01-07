0cfec97f42e30b4094af98f15d4efb6f77940d3d
Lucy <lgw2@uw.edu>
2014-01-07 14:53:24 -0800

    fixed bug in toDelete

--- computeNewRankTop25.R
+++ computeNewRankTop25.R
@@ -11,10 +11,10 @@ computeNewRank <- function(newGamesDF) {
   teams <- unique(c(newGamesDF$WinningTeam, newGamesDF$LosingTeam))
   pointTotals <- data.frame(TeamPoints = rep(0, length(teams)), GamesPlayed = rep(0, length(teams)) )
   rownames(pointTotals) <- teams
-  pointTotals <- fillPointTotals(newGamesDF, pointTotals)
+  #pointTotals <- fillPointTotals(newGamesDF, pointTotals)
   
   
-  for (x in 1:300) {
+  for (x in 500) {
     resultList <- computeNewAvePoints(newGamesDF, pointTotals)
     newGamesDF <- resultList$a
     pointTotals <- resultList$b
@@ -42,7 +42,6 @@ fillPointTotals<- function(newGamesDF, pointTotals) {
 computeNewAvePoints <- function(newGamesDF, pointTotals) {
   
   originalPointTotals <- pointTotals
-  print("Saved original point totals")
   
   # start with a data frame containing info for all games: winner, loser, winner score, 
   # loser score, current average points earned per game for winning team, current  average
@@ -54,51 +53,33 @@ computeNewAvePoints <- function(newGamesDF, pointTotals) {
   LosingPointsEarned <- newGamesDF$WinningPointsInitial - 400/(pmax(2/3,
                             2.5*(newGamesDF$LoserScore/newGamesDF$WinnerScore)^2))
   
-  print("Computed points earned for every game")
-  
   # put those new points into the newGames data frame as WinningPointsEarned and 
   # LosingPointsEarned
   newGamesDF <- data.frame(newGamesDF, WinningPointsEarned, LosingPointsEarned)
-  
-  print("Put points earned for every game into new games data frame")
-  
+    
   # update total current points for each team from wins
   pointTotals <- addAllWinningGames(newGamesDF, pointTotals)
-  
-  print("updated toatl current points for each team in pointTotals from winning")
-  
+    
   # update total current points for each team from losses
   pointTotals <- addAllLosingGames(newGamesDF, pointTotals)
-  
-  print("updated total current points for each team in pointTotals for losing")
-  
+    
   # divide point totals by games played to get a current average points per game for
   # each team, and add it to the pointTotals data frame
   pointTotals <- data.frame(pointTotals, pointTotals$TeamPoints/pointTotals$GamesPlayed)
-  
-  print("averaged points per game")
-  
+    
   # update winning points initial
   newGamesDF <- updateWinningPointsInitial(newGamesDF, pointTotals)
-  
-  print("updated winning points initial in new games data frame")
-  
+    
   # update losing points initial
   newGamesDF <- updateLosingPointsInitial(newGamesDF, pointTotals)
-  
-  print("updated losing points initial in new games data frame")
-  
+    
   # delete earned points columns
   newGamesDF$WinningPointsEarned <- NULL
   newGamesDF$LosingPointsEarned <- NULL
-  
-  print("delted earnred points columns")
-  
+    
   # restore pointTotals to original
   pointTotals <- data.frame(originalPointTotals, pointTotals[length(pointTotals)])
-  
-  print("Restored pointsTotals to original")
-  
+    
   # we now have a data frame with columns for WinningTeam, LosingTeam, WinnerScore, LoserScore,
   # WinningPointsInitial, LosingPointsInitial.
   # Winning and Losing points initial are now the current average points per game for the winning
@@ -154,11 +135,14 @@ deleteAllLetterScores <- function(df) {
   for (index in 1:length(rownames(df))) {
     if (is.na(df[index, "WinnerScore"]) | is.na(df[index, "LoserScore"])) {
       toDelete <- c(toDelete, index)
-      print(index)
     } 
   }
-  toDelete <- toDelete*-1
-  return(df[toDelete,])
+  if (length(toDelete) > 0) {
+    toDelete <- toDelete*-1
+    return(df[toDelete,])
+  } else {
+    return(df)
+  }
 }
 
 DFCleanup <- function(df) {
@@ -170,7 +154,6 @@ DFCleanup <- function(df) {
   columns <- c("WinningTeam", "LosingTeam", "WinnerScore", "LoserScore")
   for (index in 1:4) {
     column <- columns[index]
-    print(column)
     if (!(column %in% names(df))) {
       print(paste(column, "is missing from the data frame."))
     } else {
@@ -181,14 +164,13 @@ DFCleanup <- function(df) {
       }
     } 
   }  
-  
+    
   df <- deleteAllLetterScores(df)
-
+  
   columns <- c("WinningPointsInitial", "LosingPointsInitial", "WinningGamesPlayed",
                "LosingGamesPlayed")
   for (index in 1:4) {
     column <- columns[index]
-    print(column)
     if (!(column %in% names(df))) {
       if (column == "WinningPointsInitial" || column == "LosingPointsInitial") {
         df <- data.frame(df, rep(1000,length(df[,1])))
